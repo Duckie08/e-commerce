3909a36dd9c0eecc7f51f343045ecd07367087bf
Duckie08 <ducpham.teamx@gmail.com>
2021-07-29 22:52:40 +0700

    install styled-components to app and create  header and hompage  styled component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import './homepage.styles.scss';
-import Directory from '../../components/directory/directory.component'
+import Directory from '../../components/directory/directory.component';
+import { HomePageContainer } from './homepage.styles';
+// import './homepage.styles.scss';
+
 const HomePage = () => (
-  <div className='homepage'>
-    <h1>Welcome to my Homepage</h1>
+  <HomePageContainer>
       <Directory />
-  </div>
+  </HomePageContainer>
 );
 
 export default HomePage;
